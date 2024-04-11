a34e606fc2befbbd6c856cbb0475598f032e6c38
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-04-11 09:03:12 +0700

    Create PakeGenericMethod.java

---
+++ PakeGenericMethod.java
@@ -0,0 +1,5 @@
+String[] names = {"Eko", "Kurniawan", "Khannedy"};
+Integer[] values = {1, 2, 3, 4, 5};
+
+System.out.println(ArrayHelper.<String>count(names));
+System.out.println(ArrayHelper.count(values));
