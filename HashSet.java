d3f42a8d97584adcf7834b9bf0a4d12710268f7e
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-04-11 08:41:24 +0700

    Create HashSet.java

---
+++ HashSet.java
@@ -0,0 +1,12 @@
+Kode: HashSet
+
+    Set<String> names = new HashSet<>();
+    names.add("Eko");
+    names.add("Kurniawan");
+    names.add("Khannedy");
+
+    for (var name : names) {
+      System.out.println(name);
+    }
+  }
+}
