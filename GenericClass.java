65e4c8de0d8ee131dcde15d2e3cbed9fb62da6be
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-04-11 12:49:47 +0700

    Create GenericClass.java

---
+++ GenericClass.java
@@ -0,0 +1,15 @@
+public class MyData<T> {
+    private T data;
+
+    public MyData(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
