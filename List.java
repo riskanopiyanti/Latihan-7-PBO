3a6840f2b1d658121838e4fcdcfcce319f0db8c4
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-04-11 08:46:45 +0700

    Create List.java

---
+++ List.java
@@ -0,0 +1,11 @@
+List<String> names = new ArrayList<>();
+// List<String> names = new LinkedList<>();
+
+names.add("Eko");
+names.add("Kurniawan");
+names.add("Khannedy");
+
+names.set(0, "Programmer");
+
+System.out.println(names.get(0));
+System.out.println(names.get(1));
