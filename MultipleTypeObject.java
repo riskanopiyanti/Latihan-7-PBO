8dd0af6eeb50404dbdf41ce9b5ae16acf98b3baa
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-04-11 12:44:25 +0700

    Create MultipleTypeObject.java

---
+++ MultipleTypeObject.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+    Pair<String, Integer> pair = new Pair<String, Integer>("Eko", 28);
+    
+    System.out.println(pair.getFirst());
+    System.out.println(pair.getSecond());
+}
