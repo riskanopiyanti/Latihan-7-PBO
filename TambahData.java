004bd89b489368bb000df544f3f645770aae434b
riskanopiyanti <114473650+riskanopiyanti@users.noreply.github.com>
2024-04-11 08:56:08 +0700

    Create TambahData.java

---
+++ TambahData.java
@@ -0,0 +1,11 @@
+  Collection<String> names = new ArrayList<>();
+  names.add("Eko");
+  names.add("Kurniawan");
+  names.addAll(Arrays.asList("Kurniawan", "Programmer", "Zaman", "Now"));
+
+  for (var name: names) {
+    System.out.println(name);
+  }
+
+ }
+}
